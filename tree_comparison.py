7f20413ea93aabbf70e38bab085b3ef9800ca3ee
chris-thoene <cthoene@students.uni-mainz.de>
2024-11-21 10:48:06 +0100

    Introducing ‘prototype_tree’, a function that automatically creates the prototypical tree for given split variables. It uses a dictionary that contains model trees for one, two or three splits.

--- tree_comparison.py
+++ tree_comparison.py
@@ -89,7 +89,11 @@ def compare_nodes(true_node, test_node, sampi_score_tracker):
                 if (type(split_value_1) == str) and (split_value_1 != split_value_2):       # Muster_Node ist "str", aber Test_Node ist nicht identisch
                     sampi_score_tracker['sampi_score']      += 1
                 elif (type(split_value_1) != str) and (type(split_value_2) != str):         # Muster_Node und Test_Node sind Zahlen
-                    sampi_score_tracker['sampi_score']      += abs(split_value_1 - split_value_2)   # Strafpunkte für Unterschied in Split-Werten
+                    sampi_score_tracker['sampi_score']      += min(abs(split_value_1 - split_value_2)*2, 1)
+                            # Strafpunkte für Unterschied in Split-Werten
+                            # Der designierte Split ist bei 0.5 (bzw. 0.1); die Werte verlaufen zwischen 0 und 1
+                            # Daher wird die Differenz verdoppelt.
+                            # (jedoch maximal 1 für den letzten Split mit 0.1 als designierte Grenze)
                 elif (type(split_value_1) != str) and (type(split_value_2) == str):         # Muster_Node ist Zahl, aber Test_Node nicht.
                     sampi_score_tracker['sampi_score']      += 1
 
@@ -107,14 +111,12 @@ def compare_nodes(true_node, test_node, sampi_score_tracker):
         else: # If 'left' only exists in true_node
             compare_nodes(true_node['right'], {'Node': {'Split': False}}, sampi_score_tracker)
 
-
-
 # Funktion zur Berechnung des Sampi-Werts. (Similarity of A Manifested and Prototype-tree Indicator)
-def calc_sampi(tree1, tree2):
+def calc_sampi(true_tree, test_tree):
     sampi_score_tracker = {'sampi_score': 0, 'n_nodes': 0}
 
     # Vergleich der Wurzelknoten starten
-    compare_nodes(tree1, tree2, sampi_score_tracker)
+    compare_nodes(true_tree, test_tree, sampi_score_tracker)
 
     # Sampi_Score hinsichtlich der Gesamtzahl an untersuchten Knoten normieren (Sampi berechnen).
     sampi_score = sampi_score_tracker['sampi_score']
@@ -122,4 +124,138 @@ def calc_sampi(tree1, tree2):
     sampi = sampi_score / n_nodes if n_nodes > 0 else 0
     return sampi
 
-print("\u03e1 = ", calc_sampi(Muster, Test))
+#print("\u03e1 = ", calc_sampi(Muster, Test))
+
+
+
+#_______________________________________________________________________________________________________________________
+#_______________________________________________________________________________________________________________________
+# A Directory of all Prototype Trees
+
+# Vorlage für Bäume mit einem Split:
+one_split = {
+                 'Corr': None, 'Relationship_Var' : 'X', 'DP': 10000,
+                 'Node': {'Split': True, 'Split_Var': None, "Split_Value": None},
+                 'left':
+                     {
+                         'Corr': None, 'Relationship_Var' : 'X', 'DP': 1000,
+                         'Node': {'Split': False}
+                     },
+                'right':
+                    {
+                        'Corr': None, 'Relationship_Var' : None, 'DP': 9000,
+                        'Node': {'Split': False}
+                    }
+              }
+
+# Vorlage für Bäume mit zwei Splits:
+two_split = {
+                'Corr': None, 'Relationship_Var': 'X', 'DP' : 20000,
+                'Node' : {'Split': True, 'Split_Var': None, 'Split_Value': None},
+                'left':
+                    {
+                        'Corr': None, 'Relationship_Var': "X", 'DP': 10000,
+                        'Node': {'Split': True, 'Split_Var': None, 'Split_Value': None},
+                        'left':
+                             {
+                                'Corr': None, 'Relationship_Var': "X", 'DP': 1000,
+                                'Node': {'Split': False}
+                             },
+                        'right':
+                             {
+                                 'Corr': None, 'Relationship_Var': None, 'DP': 9000,
+                                 'Node': {'Split': False}
+                             }
+                     },
+                'right':
+                    {
+                        'Corr': None, 'Relationship_Var': None, 'DP': 10000,
+                        'Node': {'Split': False}
+                    }
+            }
+
+# Vorlage für Bäume mit drei Splits:
+three_split = {
+                'Corr': None, 'Relationship_Var': 'X', 'DP' : 40000,
+                'Node' : {'Split': True, 'Split_Var': None, 'Split_Value': None},
+                'left':
+                    {
+                        'Corr': None, 'Relationship_Var': "X", 'DP': 20000,
+                        'Node': {'Split': True, 'Split_Var': None, 'Split_Value': None},
+                        'left':
+                             {
+                                'Corr': None, 'Relationship_Var': "X", 'DP': 10000,
+                                'Node': {'Split': True, 'Split_Var': None, 'Split_Value': None},
+                                'left':
+                                    {
+                                        'Corr': None, 'Relationship_Var': "X", 'DP': 1000,
+                                        'Node': {'Split': False}
+                                    },
+                                'right':
+                                    {
+                                        'Corr': None, 'Relationship_Var': None, 'DP': 9000,
+                                        'Node': {'Split': False}
+                                    },
+                             },
+                        'right':
+                             {
+                                 'Corr': None, 'Relationship_Var': None, 'DP': 10000,
+                                 'Node': {'Split': False}
+                             }
+                     },
+                'right':
+                    {
+                        'Corr': None, 'Relationship_Var': None, 'DP': 20000,
+                        'Node': {'Split': False}
+                    }
+            }
+
+
+def prototype_trees(var1, var2, var3):
+    if var1 == "O":  # Two or one splits
+        if var2 == "O": # Tree with a single split
+            tree = one_split
+            tree['Node']['Split_Var'] = var3
+            if var3 == "Kt":  # Categorical Split
+                tree["Node"]['Split_Value'] = "A"
+            else:             # Continuous Split (if var3 = X, Y or Kn)
+                tree["Node"]['Split_Value'] = 0.1
+
+        else: #Tree with two splits
+            tree = two_split
+            tree['Node']['Split_Var'] = var2
+            if var2 == "Kt":  # Categorical Split
+                tree["Node"]['Split_Value'] = "B"
+            else:             # Continuous Split (if var2 = X, Y or Kn)
+                tree["Node"]['Split_Value'] = 0.5
+
+            tree['left']['Node']['Split_Var'] = var3
+            if var3 == "Kt":  # Categorical Split
+                tree['left']['Node']['Split_Value'] = "A"
+            else:             # Continuous Split (if var3 = X, Y or Kn)
+                tree['left']['Node']['Split_Value'] = 0.1
+
+    else: #Tree with three splits
+        tree = three_split
+        tree['Node']['Split_Var'] = var1
+        if var1 == "Kt":  # Categorical Split
+            tree["Node"]['Split_Value'] = "C"
+        else:  # Continuous Split (if var2 = X, Y or Kn)
+            tree["Node"]['Split_Value'] = 0.5
+
+        tree['left']['Node']['Split_Var'] = var2
+        if var2 == "Kt":  # Categorical Split
+            tree['left']['Node']['Split_Value'] = "B"
+        else:  # Continuous Split (if var3 = X, Y or Kn)
+            tree['left']['Node']['Split_Value'] = 0.5
+
+        tree['left']['left']['Node']['Split_Var'] = var3
+        if var3 == "Kt":  # Categorical Split
+            tree['left']['left']['Node']['Split_Value'] = "A"
+        else:  # Continuous Split (if var3 = X, Y or Kn)
+            tree['left']['left']['Node']['Split_Value'] = 0.1
+
+    return tree
+
+
+#test= prototype_trees("Y", "Kt", "X")
